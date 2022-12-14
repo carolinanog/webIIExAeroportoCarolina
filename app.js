18fc825cae730932c995c4fa904f79e5b42aebc6
Carolina Nogueira <carolina.tinen@alumni.usp.br>
2022-12-14 14:45:50 -0300

    update port

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import pkg from 'dotenv';
 import { routes } from './routes.js';
 import path from 'path';
+
 const { PORT } = pkg.config().parsed; 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -14,13 +15,3 @@ app.listen(PORT, () => {
     console.log(`Server is running at port ${PORT}.`);
     console.log(`Acesse http://localhost/viaje`)
 });
-
-
-
-//   <%row.forEach(aeroporto => {%> 
-//   <div class="card" style="width: 18rem;">
-//   <img class="card-img-top" src="img/<%=aeroporto.imagem%>" alt="<%=aeroporto.nome%>">
-//   <div class="card-body">
-//     <h5 class="card-title"><%=aeroporto.nome%></h5>
-//     <p class="card-text"><%=aeroporto.cidade%> - <%=aeroporto.UF%></p>
-//      Código IATA: <%=aeroporto.cod_IATA%></div>
